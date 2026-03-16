81bef0254b611fc43bdc80d004afd41afb40ef1f
Tigran Gyozalyan <44705926+TigranGyozalyan@users.noreply.github.com>
2026-03-16 18:09:36 +0400

    Update create_user_tool.py

--- mcp_server/tools/users/create_user_tool.py
+++ mcp_server/tools/users/create_user_tool.py
@@ -8,21 +8,17 @@ class CreateUserTool(BaseUserServiceTool):
 
     @property
     def name(self) -> str:
-        #TODO: Provide tool name as `add_user`
-        raise NotImplementedError()
+        return 'add_user'
 
     @property
     def description(self) -> str:
-        #TODO: Provide description of this tool
-        raise NotImplementedError()
+        return 'Create a new user based on the provided information'
 
     @property
     def input_schema(self) -> dict[str, Any]:
-        #TODO: Provide tool params Schema. To do that you can create json schema from UserCreate pydentic model ` UserCreate.model_json_schema()`
-        raise NotImplementedError()
+        return UserCreate.model_json_schema()
 
     async def execute(self, arguments: dict[str, Any]) -> str:
-        #TODO:
-        # 1. Validate arguments with `UserCreate.model_validate`
-        # 2. Call user_client add user and return its results (it is async, don't forget to await)
-        raise NotImplementedError()
+        UserCreate.model_validate(arguments)
+        user = UserCreate(**arguments)
+        return await self._user_client.add_user(user)
